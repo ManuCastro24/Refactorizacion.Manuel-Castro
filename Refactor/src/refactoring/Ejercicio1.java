5f39578e29ab332eebe723950cd1319c2593f80f
Manuel Castro <manuelcastrofernandez.mcf@gmail.com>
2024-01-22 23:09:06 +0100

    Modificacion del ejercicio

--- Refactor/src/refactoring/Ejercicio1.java
+++ Refactor/src/refactoring/Ejercicio1.java
@@ -7,39 +7,64 @@ public class Ejercicio1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		var sc=new Scanner(System.in);
-		int eleccion;
-		int resultado;
-		System.out.println("Introduce que quieres realizar,1 sumar, 2 restar, 3 multiplicar y 4 dividir");
-		eleccion=sc.nextInt();
-		System.out.println("Introduce un numero");
-		int numero1=sc.nextInt();
-		System.out.println("introduce otro numero");
-		int numero2=sc.nextInt();
-		sc.close();
 		
-		if(eleccion==1) {
-			resultado=numero1+numero2;System.out.println(resultado);
-		}
-		else {
-			if(eleccion==2) {
-				resultado=numero1-numero2;System.out.println(resultado);
-			}
-			else {
-				if(eleccion==3) {
-					resultado=numero1*numero2;System.out.println(resultado);
-				}
-				else {
-					if(eleccion==4) {
-						resultado=numero1/numero2;System.out.println(resultado);
-					}
-					else {
-						System.out.println("Se introdujo un numero fuera de rango");
-					}
-				}
-			}
-		}
-	}
+		//Creacion de variables
+				int eleccion,int resultado;
+				
+				System.out.println("Introduce que quieres realizar,1 sumar, 2 restar, 3 multiplicar y 4 dividir");
+				eleccion=sc.nextInt();
+				
+				System.out.println("Introduce un numero");
+				int numero1=sc.nextInt();
+				
+				System.out.println("introduce otro numero");
+				int numero2=sc.nextInt();
+				
+				
+		        switch (eleccion) {
+		        
+	            case 1:
+	            	
+	                resultado = numero1 + numero2;
+	                
+	                System.out.println(resultado);
+	                
+	                break;
+	                
+	            case 2:
+	            	
+	                resultado = numero1 - numero2;
+	                
+	                System.out.println(resultado);
+	                
+	                break;
+	                
+	            case 3:
+	            	
+	                resultado = numero1 * numero2;
+	                
+	                System.out.println(resultado);
+	                
+	                break;
+	                
+	            case 4:
+	            	
+	                resultado = numero1 / numero2;
+	                
+	                System.out.println(resultado);
+	                
+	                break;
+	                
+	            case 5:
+	            	
+	                System.out.println("Se introdujo un numero fuera de rango");
+	        }
+					
+	  }
+			
+			sc.close();
 }
+	
 
 
 
